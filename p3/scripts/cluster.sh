8b327836a29efc522b905544c0951341ed433b68
Yifan Rigny <yrigny@student.42.fr>
2025-07-25 14:02:06 +0000

    fixed syntax error in deployment yaml; improved cluster setting script

--- p3/scripts/cluster.sh
+++ p3/scripts/cluster.sh
@@ -8,35 +8,38 @@ kubectl get nodes
 
 echo ">>>>>> Installing Argo CD in the k3s cluster..."
 kubectl create namespace argocd
+kubectl create namespace dev
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo ">>>>>> Waiting for Argo CD pods to be ready..."
+kubectl wait --for=condition=Available --timeout=300s -n argocd deployment/argocd-server
+kubectl wait --for=condition=Available --timeout=300s -n argocd deployment/argocd-repo-server
+kubectl wait --for=condition=Available --timeout=300s -n argocd deployment/argocd-application-controller
+kubectl wait --for=condition=Ready --timeout=300s -n argocd pod -l app.kubernetes.io/name=argocd-redis
 
 echo ">>>>>> Checking the status of Argo CD pods..."
 kubectl get pods -n argocd
 
 echo ">>>>>> Accessing the Argo CD API server by port forwarding..."
-kubectl port-forward svc/argocd-server -n argocd 8080:443 --address 0.0.0.0 &
-password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)
-argocd admin $(password) -n argocd
+kubectl port-forward svc/argocd-server -n argocd 8080:443 --address 0.0.0.0 2>/dev/null &
+PF_PID=$!
+sleep 3
 
-echo ">>>>>> Changing the password of admin for Argo CD..."
-new_password=yifanrigny
-sudo apt install -y expect
-expect <<EOF
-spawn argocd account update-password
-expect "*** Enter password of currently logged in user (admin):"
-send "$password\r"
-expect "*** Enter new password for user admin:"
-send "$new_password\r"
-expect "*** Confirm new password for user admin:"
-send "$new_password\r"
-expect eof
-EOF
+echo ">>>>>> Extracting the password of admin for Argo CD..."
+password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)
+printf "\033[0;32mPassword: $password\n\033[0m"
+
+echo ">>>>>> Logging in Argo CD admin account..."
+argocd login localhost:8080 --username admin --password "$password" --insecure
 
 echo ">>>>>> Creating the playground example app..."
 argocd app create playground \
-	--repo https://github.com/yrigny/42-IoT \
-	--path playground \
+	--repo https://github.com/yrigny/42-IoT.git \
+	--path ./p3/playground \
 	--dest-server https://kubernetes.default.svc \
 	--dest-namespace dev \
-	--sync-policy auto
+	--sync-policy auto \
+	--insecure
 
+echo ">>>>>> Cleaning up port-forwarding..."
+kill $PF_PID
